896833f32cfeede4eb038704d4843310b8791bb2
Meet Patel <meet.s.patel.nj@gmail.com>
2025-02-13 22:00:47 -0500

    Trained model on vastai. TODO: visualizations

--- deep_reorder/deep_reorder.py
+++ deep_reorder/deep_reorder.py
@@ -1,55 +1,39 @@
+"""Deep Reorder is an experiment for reordering MLP blocks based on activation correlation."""
+
 from typing import Tuple, Callable
+import math
 
 import torch
 from torch import nn
 
 batched_corrcoef = torch.func.vmap(torch.corrcoef)
+# batched_corrcoef = torch.compile(torch.func.vmap(torch.corrcoef), backend='eager')
 
 
-def register_buffers(model: nn.Module, device: torch.device | str = "cpu"):
+def register_buffers(model: nn.Module):
     """Register necessary buffers and parameters in the model.
 
     Args:
         model (nn.Module):
             model to add buffers and parameters to.
-        device (torch.device or str):
-            device to store buffers on.
     """
     for idx, layer in enumerate(model.layers):
         idx: int
         layer: nn.Module
-        linear_positions = nn.Parameter(torch.linspace(0, 1, layer.hidden_size, dtype=torch.float32, device=device))
-        activation_correlations = torch.zeros((layer.hidden_size, layer.hidden_size), dtype=torch.float32, device=device)
+        linear_positions = nn.Parameter(math.sqrt(layer.hidden_size) * torch.rand(layer.hidden_size, requires_grad=True, dtype=torch.float32))
+        original_positions = linear_positions.detach()
+
+        sorted_indices = torch.argsort(linear_positions)
+        ranked_positions = torch.zeros_like(linear_positions, dtype=torch.long)
+        ranked_positions[sorted_indices] = torch.arange(layer.hidden_size)
+        colors = nn.functional.normalize(ranked_positions.float(), dim=-1)
+        colors.requires_grad = False
+
+        correlation_matrix = torch.zeros((layer.hidden_size, layer.hidden_size), requires_grad=False, dtype=torch.float32)
+        layer.register_buffer("colors", colors)
         layer.register_parameter("linear_positions", linear_positions)
-        layer.register_buffer("activation_correlations", activation_correlations)
-
-
-def _save_activation_hook(_module_name: str) -> Callable[[nn.Module, Tuple[torch.Tensor], Tuple[torch.Tensor]], None]:
-    """Generate hook to attach to modules.
-
-    Args:
-        _module_name (str):
-            name of the module.
-
-    Returns:
-        A hook that computes and stores correlation matrices for activations of a module.
-    """
-
-    def hook(module: nn.Module, _input_tensor: Tuple[torch.Tensor], output_tensor: Tuple[torch.Tensor]):
-        """Compute correlation matrix for activations.
-
-        Args:
-            module (nn.Module):
-                module that is calling this hook
-            _input_tensor (Tuple[torch.Tensor]):
-                input that is fed into the module
-            output_tensor (Tuple[torch.Tensor]):
-                output from the module
-        """
-        correlation_matrix: torch.Tensor = batched_corrcoef(torch.transpose(output_tensor[0], 1, 2))
-        module.activation_correlations = torch.mean(correlation_matrix, dim=0)
-
-    return hook
+        layer.register_buffer("original_positions", original_positions)
+        layer.register_buffer("correlation_matrix", correlation_matrix)
 
 
 def register_hooks(model: nn.Module):
@@ -59,5 +43,55 @@ def register_hooks(model: nn.Module):
         model (nn.Module):
             model to add hooks to.
     """
+
+    def save_activation_hook(_module_name: str) -> Callable[[nn.Module, Tuple[torch.Tensor], Tuple[torch.Tensor]], None]:
+        """Generate hook to attach to modules.
+
+        Args:
+            _module_name (str):
+                name of the module.
+
+        Returns:
+            A hook that computes and stores correlation matrices for activations of a module.
+        """
+
+        def hook(module: nn.Module, _input_tensor: Tuple[torch.Tensor], output_tensor: Tuple[torch.Tensor]):
+            """Compute correlation matrix for activations and store in buffer.
+
+            Args:
+                module (nn.Module):
+                    module that is calling this hook
+                _input_tensor (Tuple[torch.Tensor]):
+                    input that is fed into the module
+                output_tensor (Tuple[torch.Tensor]):
+                    output from the module
+            """
+            module.correlation_matrix = batched_corrcoef(torch.transpose(output_tensor[0], 1, 2))
+
+        return hook
+
     for idx, layer in model.layers.named_children():
-        layer.register_forward_hook(_save_activation_hook(f"{layer.__class__.__name__}_{idx}"))
+        layer.register_forward_hook(save_activation_hook(f"{layer.__class__.__name__}_{idx}"))
+
+
+@torch.compile(backend="eager")
+def _compute_distance_matrix(positions: torch.Tensor) -> torch.Tensor:
+    """Compute pairwise distance matrix, given a positions vector."""
+    col_vec = positions.unsqueeze(1)
+    row_vec = positions.unsqueeze(0)
+    return torch.abs(col_vec - row_vec)
+
+
+@torch.compile(backend="eager")
+def _compute_layer_loss(layer: nn.Module) -> torch.Tensor:
+    """Compute the loss for a layer."""
+    distance_matrix = _compute_distance_matrix(layer.linear_positions)
+    target_distance_matrix = torch.clamp(1 - layer.correlation_matrix, min=0)
+    loss_matrix = (distance_matrix - target_distance_matrix) ** 2
+    return torch.mean(loss_matrix)
+
+
+@torch.compile(backend="eager")
+def compute_model_loss(model: nn.Module) -> torch.Tensor:
+    """Compute the DeepReorder loss for a model."""
+    return torch.mean(torch.stack([_compute_layer_loss(layer) for _, layer in model.layers.named_children()]))
--- deep_reorder/train.py
+++ deep_reorder/train.py
@@ -1,48 +1,133 @@
+"""Training script for DeepReorder."""
+
+import argparse
+import os
+import io
+
+import numpy as np
 import torch
+import accelerate
+from torch import nn
 from torch.utils.data.dataloader import DataLoader
-from transformers import AutoTokenizer, AutoModelForCausalLM
-from transformers.models.qwen2.modeling_qwen2 import Qwen2ForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig
 from datasets import load_dataset
-from tqdm import tqdm
 
-from deep_reorder import register_buffers, register_hooks
+from tqdm.auto import tqdm
+import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
 
-if torch.cuda.is_available():
-    DEVICE = "cuda"
-elif torch.mps.is_available():
-    DEVICE = "mps"
-else:
-    DEVICE = "cpu"
-print(f"Using {DEVICE}.")
+import deep_reorder
+
+DEFAULT_MODEL = "Qwen/Qwen2.5-0.5B"
+DEFAULT_DATASET = "oivlisnet/c4-en"
 
 
-def main():
+def main(model_name: str, dataset_name: str):
+    """Run the main training loop."""
+    accelerator = accelerate.Accelerator(gradient_accumulation_steps=8)
+    device = accelerator.device
+
+    log_dir = "runs/deepreorderexperiment"
+    writer = SummaryWriter(log_dir=log_dir)
+
+    checkpoint_dir = os.path.join(log_dir, "checkpoints")
+    os.makedirs(checkpoint_dir, exist_ok=True)  # Create directory if it doesn't exist
+
     # Load model and freeze weights.
-    model: Qwen2ForCausalLM = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-3B", torch_dtype="bfloat16", device_map="auto")
+    model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="bfloat16", device_map="auto")
     model.model.eval()
     for param in model.model.parameters():
         param.requires_grad = False
 
     # Create necessary buffers.
-    register_buffers(model.model)
-    register_hooks(model.model)
+    deep_reorder.register_buffers(model.model)
+    deep_reorder.register_hooks(model.model)
+    model = torch.compile(model, backend="eager")
+    model.to(device)
 
     # Load tokenizer.
-    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-3B")
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    # DATA LOADING CODE HERE
-    train_dataset = load_dataset("sedthh/gutenberg_english", split="train")
-    train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+    # Set up dataloaders.
+    train_dataset = load_dataset(dataset_name, split="train").select_columns(["text"])
+    train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
 
-    train_bar = tqdm(train_dataloader, desc=f"[TRAIN] ")
-    for batch in train_bar:
-        batch = tokenizer(batch["TEXT"], padding=True, padding_side="left", truncation=True, return_tensors="pt").to(DEVICE)
-        _ = model.generate(batch.input_ids, attention_mask=batch.attention_mask, do_sample=False, pad_token_id=tokenizer.pad_token_id)
-        loss = torch.sum(model.model.layers[0].activation_correlations.to(DEVICE) @ model.model.layers[0].linear_positions.to(DEVICE))
-        loss.backward()
+    optimizer = torch.optim.AdamW(model.model.parameters(), lr=0.1)
 
-        # TODO: Need to add optimizer.
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, factor=0.1)
+
+    # initial_lr_scheduler = torch.optim.lr_scheduler.LinearLR(optimizer=optimizer, start_factor=1, end_factor=0.1, total_iters=500)
+    # warmup_lr_scheduler = torch.optim.lr_scheduler.LinearLR(optimizer=optimizer, start_factor=0.001, end_factor=0.1, total_iters=50)
+
+    model, optimizer, scheduler, train_dataloader = accelerator.prepare(model, optimizer, scheduler, train_dataloader)
+
+    train_bar = tqdm(train_dataloader, desc=f"[TRAIN] ", disable=not accelerator.is_local_main_process)
+    for idx, batch in enumerate(train_bar):
+        with accelerator.accumulate(model):
+            # Tokenize.
+            batch = batch["text"]
+            batch = tokenizer(
+                batch,
+                padding=True,
+                padding_side="left",
+                truncation=True,
+                max_length=AutoConfig.from_pretrained(model_name).max_position_embeddings,
+                return_tensors="pt",
+            )
+            batch.to(device)
+
+            # Run forward pass. Don't really care about what token was generated, so discard it.
+            _ = model.generate(batch.input_ids, attention_mask=batch.attention_mask, do_sample=False, max_new_tokens=1, pad_token_id=tokenizer.pad_token_id)
+            loss = deep_reorder.compute_model_loss(model.model)
+            train_bar.write(f"{loss.item(): >.3f}")
+
+            accelerator.backward(loss)
+
+            optimizer.step()
+            scheduler.step(loss)
+            optimizer.zero_grad()
+
+            # Log Metrics
+            writer.add_scalar("Loss/train", loss.item(), idx)
+            writer.add_scalar("Learning_Rate", optimizer.param_groups[0]["lr"], idx)
+
+            # Log Charts
+            if idx % 100 == 0:
+                fig, axes = plt.subplots(len(model.model.layers), sharex=True, figsize=(20, 20))
+                fig.tight_layout()
+                for layer_idx, layer in enumerate(model.model.layers):
+                    axes[layer_idx].clear()
+                    axes[layer_idx].scatter(
+                        layer.linear_positions.detach().cpu().numpy(),
+                        torch.zeros_like(layer.linear_positions).detach().cpu().numpy(),
+                        c=layer.colors.cpu().numpy(),
+                        s=1,
+                    )
+                    axes[layer_idx].set_yticks([])
+                    axes[layer_idx].set_ylabel(f"{layer_idx}")
+                buf = io.BytesIO()
+                plt.savefig(buf, format="png")
+                buf.seek(0)
+                image = np.array(plt.imread(buf))
+                image = np.transpose(image, (2, 0, 1))
+                writer.add_image("Linear Positions Scatter Plots", image, idx)
+                plt.close(fig)
+
+            # Checkpoint
+            if idx % 2500 == 0 and idx != 0:
+                accelerator.wait_for_everyone()
+                output_checkpoints_dir = os.path.join(checkpoint_dir, f"step_{idx}")
+                accelerator.save_state(output_checkpoints_dir)
+                print(f"Checkpoint saved at step {idx} to {output_checkpoints_dir}")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(prog="DeepReorder")
+    parser.add_argument("--model", type=str, default=DEFAULT_MODEL)
+    parser.add_argument("--dataset", type=str, default=DEFAULT_DATASET)
+    args = parser.parse_args()
+
+    main(
+        model_name=args.model,
+        dataset_name=args.dataset,
+    )
